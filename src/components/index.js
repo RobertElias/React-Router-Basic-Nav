1a7fc444c5312b3f1524c3c5bc99d56174f0c17e
Robert <Robert.Elias@live.com>
2019-12-16 13:00:30 -0800

    imported BrowswerRouter

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
-import Home from './Home';
+import { Link, Route, BrowserRouter as Router } from "react-router-dom";
+
 import About from './About';
 import Contact from './Contact';
+import Home from './Home';
 import Navigation from './Navigation';
 
 export { Home, About, Contact, Navigation };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from "react-router-dom"
+
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
